aa3ca19ef4c814fc40f1a0ca4e05fdc00c18cb1d
6finger <kvasnicka.simon@gmail.com>
2017-08-07 06:35:28 +0200

    added main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+'use strict'
+
+let Canvas = require('./app/models/Canvas');
+let Renderer = require('./app/models/Renderer');
+let View = require('./app/views/View');
+let Dispatcher = require('./app/controllers/Dispatcher');
+let Controller = require('./app/controllers/Controller');
+let CommandFactory = require('./app/commands/CommandFactory');
+
+console.log('start');
+
+if (!process.stdin || !process.stdout) {
+    return;
+}
+
+let view = new View(process.stdin, process.stdout);
+let canvas = new Canvas();
+let renderer = new Renderer(canvas);
+let controller = new Controller();
+let commandFactory = new CommandFactory(canvas, renderer, view);
+let dispatcher = new Dispatcher(controller, commandFactory);
+
+canvas.maxWidth = ( process.stdout.columns != null ? process.stdout.columns : 80 )
+    - 2*renderer.verticalBorderWidth;
+canvas.maxHeight = ( process.stdout.rows != null ? process.stdout.rows : 80 )
+    - 2*renderer.horizontalBorderWidth;
+
+function run() {
+    dispatcher.dispatch(controller.getNextCommand());
+}
+
+run();
+
+module.exports = run;
